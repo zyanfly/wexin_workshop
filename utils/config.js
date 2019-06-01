ba92f8044f471d3ead9ef692dc6250fb5bdc0f8e
jayzen <jayzen@foxmail.com>
2019-06-01 10:39:51 +0800

    do some fix

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    // api_base_url: 'https://eco-news.cn/api/v1/',
-    // appkey: "t9f726cgrp"
-    api_base_url: 'http://localhost:3000/api/v1/',
-    appkey: "4388x2fgs5"
+    api_base_url: 'https://eco-news.cn/api/v1/',
+    appkey: "kdp13rqi08"
+    // api_base_url: 'http://localhost:3000/api/v1/',
+    // appkey: "4388x2fgs5"
 }
 
 export { config }
