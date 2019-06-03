94f9f6874333b583bfc4348b748703cc436f2ccf
anfly <zyanfly1832@163.com>
2019-06-03 13:05:17 +0800

    完成

--- components/carousel-tap/index.js
+++ components/carousel-tap/index.js
@@ -4,8 +4,8 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    carousels:{
-      type:Array
+    carousels: {
+      type: Array
     }
   },
 
@@ -13,38 +13,24 @@ Component({
    * 组件的初始数据
    */
   data: {
-    toView:'item0'
+    toView: 'item0'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    scrollView(e){
-      var index = this.getIndex(this.data.toView);
+    viewImage(e){
       console.log(e)
-      // for(var i = 0;i<this.properties.carousels.length;i++){
-        if(e.detail.deltaX<0){
-          this.setData({
-            toView:'item'+(index+1)
-          })
-        }else{
-          this.setData({
-            toView:'item'+(index-1)
-          })
-        }
-      // }
-      return false;
-    },
-    getIndex(str){
-      var index = str.lastIndexOf('m');
-      str = str.substring(index+1,str.length);
-      return +str;
-    },
-    change(){
-      console.log(11)
-      this.setData({
-        toView:'item2'
+      let arr = [];
+      for(let item of this.properties.carousels){
+        arr.push(item.pic)
+      }
+      console.log(arr)
+      var src = e.currentTarget.dataset.src;
+      wx.previewImage({
+        current:src,
+        urls:arr
       })
     }
   }
